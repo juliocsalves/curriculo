083218aefdcf83f599e433b82d9fbfb438efcfff
Julio Cezar da Silva Alves <82619071+juliocsalves@users.noreply.github.com>
2025-03-08 22:34:20 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,13 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/juliocsalves/curriculo/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const url = 'https://raw.githubusercontent.com/juliocsalves/curriculo/main/data/profile.json';    
+    try {
+        const response = await fetch(url);                
+        if (!response.ok) {
+            throw new Error('Erro ao carregar o perfil: ' + response.status);
+        }        
+        const profileData = await response.json();
+        return profileData;
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+    }
 }
