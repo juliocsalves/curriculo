b38e2ac3f2b8fa8ad0aae6a11eefd0eaef4295cf
juliocsalves <juliocsalves@gmail.com>
2025-03-30 17:36:20 -0300

    Versão atualizada em 30-03-25

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,103 @@
+// Função genérica para atualizar um elemento
+function updateElement(elementId, value, isHtml = false) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        if (isHtml) {
+            element.innerHTML = value;
+        } else {
+            element.innerText = value;
+        }
+    }
+}
 
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
+    updateElement('profile.photo', profileData.photo);
+    const photo = document.getElementById('profile.photo');
+    if (photo) {
+        photo.alt = profileData.name;
+    }
 
-    const name = document.getElementById('profile.name')
-    name.innerText = profileData.name
+    updateElement('profile.name', profileData.name);
+    updateElement('profile.job', profileData.job);
+    updateElement('profile.location', profileData.location);
+    updateElement('profile.about', profileData.about, true);
+    updateElement('profile.pcd', profileData.pcd);
+    updateElement('profile.phone', profileData.phone);
+    updateElement('profile.site', profileData.site);
 
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
+    const phone = document.getElementById('profile.phone');
+    if (phone) {
+        phone.href = `tel:${profileData.phone}`;
+    }
 
-    const location = document.getElementById('profile.location')
-    location.innerText = profileData.location
+    updateElement('profile.email', profileData.email);
+    const email = document.getElementById('profile.email');
+    if (email) {
+        email.href = `mailto:${profileData.email}`;
+    }
 
-    const about = document.getElementById('profile.about')
-    about.innerHTML = profileData.about
-
-    const phone = document.getElementById('profile.phone')
-    phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
-
-    const email = document.getElementById('profile.email')
-    email.innerText = profileData.email
-    email.href = `mailto:${profileData.email}`
+    // Atualiza o texto para PCD
+    updateElement('profile.pcd', profileData.pcd);
 }
 
-function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile.skills.softSkills')
-    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
-}
+function updateSkills(profileData) {
+    const updateSkillsList = (elementId, skills, isHardSkills = true) => {
+        const skillsElement = document.getElementById(elementId);
+        if (skillsElement) {
+            const skillsHtml = skills.map(skill => {
+                if (isHardSkills) {
+                    return `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`;
+                } else {
+                    return `<li>${skill}</li>`;
+                }
+            }).join('');
+            skillsElement.innerHTML = skillsHtml;
+        }
+    };
 
-function updateHardSkills(profileData) {
-    const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+    updateSkillsList('profile.skills.hardSkills', profileData.skills.hardSkills, true);
+    updateSkillsList('profile.skills.softSkills', profileData.skills.softSkills, false);
 }
 
 function updateLanguages(profileData) {
-    const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+    updateElement('profile.languages', profileData.languages.map(language => `<li>${language}</li>`).join(''), true);
 }
 
 function updatePortfolio(profileData) {
-    const portfolio = document.getElementById('profile.portfolio')
-    portfolio.innerHTML = profileData.portfolio.map(project => {
-        return `
-            <li>
-                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>                
-                <a href="${project.url}" target="_blank">Clique aqui para visualizar o projeto.</a>
-            </li>
-        `
-    }).join('')
+    const portfolioElement = document.getElementById('profile.portfolio');
+    if (portfolioElement) {
+        portfolioElement.innerHTML = profileData.portfolio.map(project => {
+            return `
+                <li>
+                    <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>                
+                    <a href="${project.url}" target="_blank">Clique aqui para visualizar o código do projeto projeto.</a>
+                    <a href="${project.site}" target="_blank">Ver protótipo rodando. </a>
+                </li>             
+            `;
+        }).join('');
+    }
 }
 
 function updateProfessionalExperience(profileData) {
-    const professionalExperience = document.getElementById('profile.professionalExperience')
-    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
-        return `
-            <li>
-                <h3 class="title">${experience.name}</h3>
-                <p class="period">${experience.period}</p>
-                <p>${experience.description}</p>
-            </li>
-        `
-    }).join('')
+    const professionalExperienceElement = document.getElementById('profile.professionalExperience');
+    if (professionalExperienceElement) {
+        professionalExperienceElement.innerHTML = profileData.professionalExperience.map(experience => {
+            return `
+                <li>
+                    <h3 class="title">${experience.name}</h3>
+                    <p class="period">${experience.period}</p>
+                    <p>${experience.description}</p>
+                </li>
+            `;
+        }).join('');
+    }
 }
 
 (async () => {
-    const profileData = await fetchProfileData()
-    updateProfileInfo(profileData)
-    updateSoftSkills(profileData)
-    updateHardSkills(profileData)
-    updateLanguages(profileData)
-    updatePortfolio(profileData)
-    updateProfessionalExperience(profileData)
-})()
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+    updateSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolio(profileData);
+    updateProfessionalExperience(profileData);
+})();
